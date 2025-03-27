eb90831401dcf31432d83366e16126a59c29f778
monkwhosoldpen <monkwhosoldpen@gmail.com>
2025-03-28 05:02:55 +0530

    Update supabase.ts

--- apps/nchat/lib/supabase.ts
+++ apps/nchat/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './types/database.types';
 
-export const GlobalSupabaseUrl = "https://anktghprsqjeynpynzfs.supabase.co";
-export const GlobalsupabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFua3RnaHByc3FqZXlucHluemZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2NzQxNjAsImV4cCI6MjA1MjI1MDE2MH0.-AbMiJKycjgJ19kDl8q80JY_ufn37itC3Jdaad3uQME";
+export const GlobalSupabaseUrl = "https://risbemjewosmlvzntjkd.supabase.co";
+export const GlobalsupabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpc2JlbWpld29zbWx2em50amtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxMzIxNDIsImV4cCI6MjA1NTcwODE0Mn0._5wXtDjCr9ZnYatWD7RO5DNhx_YxUjqCcdc6qhZpwGM";
 
 // Create a single instance of the Supabase client
 export const supabase = createClient<Database>(GlobalSupabaseUrl, GlobalsupabaseAnonKey, {
