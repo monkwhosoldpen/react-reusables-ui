dd5063b9433c3c8099f49d7f7fcf6b838a700373
monkwhosoldpen <monkwhosoldpen@gmail.com>
2025-05-12 00:07:30 +0530

    test

--- apps/showcase/app/+html.tsx
+++ apps/showcase/app/+html.tsx
@@ -25,12 +25,3 @@ export default function Root({ children }: PropsWithChildren) {
     </html>
   );
 }
-
-
-import * as serviceWorkerRegistration from "../src/serviceWorkerRegistration";
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
--- apps/showcase/index.js
+++ apps/showcase/index.js
@@ -2,7 +2,6 @@ import './global.css';
 
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
-import * as serviceWorkerRegistration from './src/serviceWorkerRegistration';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
@@ -11,8 +10,3 @@ export function App() {
 }
 
 registerRootComponent(App);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
